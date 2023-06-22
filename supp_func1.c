de57e68fa99de334b939b5783b3fa6374eac2670
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-06-22 00:29:58 -0700

    task 0

---
+++ supp_func1.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+
+/**
+ * pall - print all
+ * @stack: double list
+ * @linenumber: line
+ */
+void pall(stack_t **stack, __attribute__((unused))unsigned int linenumber)
+{
+	stack_t *a = *stack;
+
+	if (!*stack)
+	{
+		return;
+	}
+	while (a)
+	{
+		printf("%d\n", a->n);
+		a = a->next;
+	}
+}
